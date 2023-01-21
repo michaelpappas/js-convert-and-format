b95ba1858c2d6052a0bbcdf86538cf8cac5308b9
michaelpappas <pappas.michael@gmail.com>
2023-01-20 21:43:16 -0800

    working with positive integer and decimal

--- convertAndFormat.js
+++ convertAndFormat.js
@@ -1,3 +1,47 @@
-function convertAndFormat() {}
+"use strict";
 
+function convertAndFormat(input) {
+  if (typeof input !== 'number') {
+    return ("That is not a number.");
+  }
+
+  if (input < 1000 && input > -1000) {
+    const numString = input.toString();
+    console.log(numString);
+    return input.toString();
+  }
+
+  if (input > 0) {
+    const wholeNum = Math.floor(input);
+    const decimal = input - wholeNum;
+    const wholeNumStr = wholeNum.toString();
+    let decimalStr;
+
+    let wholeNumArr = wholeNumStr.split("");
+    console.log("wholeNumArr-", wholeNumArr);
+    let numCommas = Math.floor(wholeNumArr.length / 3);
+    console.log("numCommas-", numCommas);
+    let revWholeNumArr = [];
+    for (let i = 0; i < wholeNumArr.length; i++) {
+      console.log("num-", wholeNumArr[i]);
+      revWholeNumArr.push(wholeNumArr[i]);
+      if (i % 3 === 0 && numCommas > 0) {
+        console.log("i-", i);
+        revWholeNumArr.push(",");
+        numCommas--;
+      }
+    }
+    console.log("finDecimalStr-", decimalStr);
+
+
+    if (Math.abs(input - wholeNum) > 0) {
+      decimalStr = (input - wholeNum).toString().substring(2);
+      return revWholeNumArr.join("") + "." + decimalStr; //need to move this logic above line 14 and handle when a negative integer is provided.
+    } else { return revWholeNumArr.join(""); }
+
+  }
+}
+
+
+convertAndFormat(process.argv[2]);
 module.exports = convertAndFormat;
--- convertAndFormat.test.js
+++ convertAndFormat.test.js
@@ -4,4 +4,16 @@ describe("#convertAndFormat", () => {
   test("it is a function", () => {
     expect(typeof convertAndFormat).toBe("function");
   });
+
+  test("converting a string returns invalid message", () => {
+    expect(convertAndFormat("tacos")).toEqual("That is not a number.");
+  });
+
+  test("converting 15", () => {
+    expect(convertAndFormat(15)).toEqual("15");
+  });
+
+  test("converting 1234", () => {
+    expect(convertAndFormat(1234)).toEqual("1,234");
+  });
 });
