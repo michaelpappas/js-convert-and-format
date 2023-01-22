99927bd21e9b99a698141235808656c5b7b92f4a
michaelpappas <pappas.michael@gmail.com>
2023-01-21 17:57:02 -0800

    everything but correct comma formatting working

--- convertAndFormat.js
+++ convertAndFormat.js
@@ -5,41 +5,52 @@ function convertAndFormat(input) {
     return ("That is not a number.");
   }
 
-  if (input < 1000 && input > -1000) {
+  if (input < 1000 && input > -1000) { // if the value doesn't require a comma then just convert toString
     const numString = input.toString();
-    console.log(numString);
     return input.toString();
   }
-
-  if (input > 0) {
-    const wholeNum = Math.floor(input);
-    const decimal = input - wholeNum;
-    const wholeNumStr = wholeNum.toString();
-    let decimalStr;
-
-    let wholeNumArr = wholeNumStr.split("");
-    console.log("wholeNumArr-", wholeNumArr);
-    let numCommas = Math.floor(wholeNumArr.length / 3);
-    console.log("numCommas-", numCommas);
-    let revWholeNumArr = [];
-    for (let i = 0; i < wholeNumArr.length; i++) {
-      console.log("num-", wholeNumArr[i]);
-      revWholeNumArr.push(wholeNumArr[i]);
-      if (i % 3 === 0 && numCommas > 0) {
-        console.log("i-", i);
-        revWholeNumArr.push(",");
-        numCommas--;
-      }
-    }
-    console.log("finDecimalStr-", decimalStr);
-
-
-    if (Math.abs(input - wholeNum) > 0) {
-      decimalStr = (input - wholeNum).toString().substring(2);
-      return revWholeNumArr.join("") + "." + decimalStr; //need to move this logic above line 14 and handle when a negative integer is provided.
-    } else { return revWholeNumArr.join(""); }
-
+  let negOffset = 0;
+  let negative;
+  if (input < 0) {
+    negative = true;
+    // console.log("negative");
+    negOffset = 1;
   }
+  let wholeNumArr = [];
+  let inputStr = input.toString();
+  let inputArr = inputStr.split("");
+  let decimalIdx = inputArr.indexOf(".");
+  if (decimalIdx >= 0) {
+    wholeNumArr = inputArr.slice(0 + negOffset, inputArr.indexOf("."));
+  }
+  else {
+    wholeNumArr = inputArr.slice(0 + negOffset, inputArr.length + negOffset);
+    console.log("negative integer-", wholeNumArr);
+    // console.log("negative state-", negative);
+    return (negative ? "-" + addCommas(wholeNumArr) : addCommas(wholeNumArr));
+  }
+
+  let decimals = inputArr.slice(decimalIdx, wholeNumArr.lenth);
+  // console.log("negative state-", negative);
+  return (negative ? "-" + addCommas(wholeNumArr) + decimals.join("", ",") : addCommas(wholeNumArr) + decimals.join("", ","));
+
+}
+
+/** takes an array of number strings and adds commas every 3 numbers starting at the end of the array (lowest value) */
+function addCommas(wholeNum) {
+  let numCommas = Math.floor(wholeNum.length / 3);
+  console.log("numCommas-", numCommas);
+  let wholeNumArr = [];
+  for (let i = 0; i < wholeNum.length; i++) {
+    console.log("num-", wholeNum[i]);
+    wholeNumArr.push(wholeNum[i]);
+    if (i % 3 === 0 && numCommas > 0) { //returns funky comma placement.
+      console.log("i-", i);
+      wholeNumArr.push(",");
+      numCommas--;
+    }
+  }
+  return wholeNumArr.join("", ",");
 }
 
 
--- convertAndFormat.test.js
+++ convertAndFormat.test.js
@@ -16,4 +16,32 @@ describe("#convertAndFormat", () => {
   test("converting 1234", () => {
     expect(convertAndFormat(1234)).toEqual("1,234");
   });
+
+  test("converting 1000000", () => {
+    expect(convertAndFormat(1000000)).toEqual("1,000,000");
+  });
+
+  test("converting 9876543210", () => {
+    expect(convertAndFormat(9876543210)).toEqual("9,876,543,210");
+  });
+
+  test("converting 6", () => {
+    expect(convertAndFormat(6)).toEqual("6");
+  });
+
+  test("converting -10", () => {
+    expect(convertAndFormat(-10)).toEqual("-10");
+  });
+
+  test("converting -5678", () => {
+    expect(convertAndFormat(-5678)).toEqual("-5,678");
+  });
+
+  test("converting 12345.678", () => {
+    expect(convertAndFormat(12345.678)).toEqual("12,345.678");
+  });
+
+  test("converting -3141592.65", () => {
+    expect(convertAndFormat(-3141592.65)).toEqual("-3,141,592.65");
+  });
 });
